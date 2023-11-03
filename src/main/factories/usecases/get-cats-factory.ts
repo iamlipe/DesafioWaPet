f2debac607bfb66cd40f520ff6b74b20af371037
Felipe Lima <felipe_lima10@icloud.com>
2023-11-03 17:08:08 -0300

    feat: add params make remote get dogs and cats

--- src/main/factories/usecases/get-cats-factory.ts
+++ src/main/factories/usecases/get-cats-factory.ts
@@ -3,11 +3,5 @@ import { RemoteGetCats } from '@/data/usecases';
 import { makeApiUrl, makeAxiosHttpClient } from '../http';
 import { BASE_CAT_API } from '@env';
 
-export const makeRemoteGetCats = (): GetCats =>
-  new RemoteGetCats(
-    makeApiUrl(BASE_CAT_API, '/images/search', {
-      limit: '10',
-      has_breeds: 'true',
-    }),
-    makeAxiosHttpClient(),
-  );
+export const makeRemoteGetCats = (params?: Record<string, string>): GetCats =>
+  new RemoteGetCats(makeApiUrl(BASE_CAT_API, '/images/search', params), makeAxiosHttpClient());
--- src/main/factories/usecases/get-dogs-factory.ts
+++ src/main/factories/usecases/get-dogs-factory.ts
@@ -3,11 +3,5 @@ import { RemoteGetDogs } from '@/data/usecases/remote-get-dogs/remote-get-dogs';
 import { makeApiUrl, makeAxiosHttpClient } from '../http';
 import { BASE_DOG_API } from '@env';
 
-export const makeRemoteGetDogs = (): GetDogs =>
-  new RemoteGetDogs(
-    makeApiUrl(BASE_DOG_API, '/images/search', {
-      limit: '10',
-      has_breeds: 'true',
-    }),
-    makeAxiosHttpClient(),
-  );
+export const makeRemoteGetDogs = (params?: Record<string, string>): GetDogs =>
+  new RemoteGetDogs(makeApiUrl(BASE_DOG_API, '/images/search', params), makeAxiosHttpClient());
